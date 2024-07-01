c29a6e80cb9588681cb31fdb44339e585f2940c3
zxstty <zhaojiaqi18@huawei.com>
2024-07-01 15:10:09 +0800

    完善语料管理脚本

--- scripts/delete_courpus.py
+++ scripts/delete_courpus.py
@@ -1,8 +1,9 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
-from sqlalchemy import create_engine, MetaData, Table, delete
+import os
+from sqlalchemy import create_engine, MetaData, Table, select,delete
 from sqlalchemy.orm import sessionmaker
 from scripts.init_all_table import KnowledgeBase, KnowledgeBaseAsset, VectorStore
-
+from scripts.query_corpus import query_corpus
 
 def delete_corpus(pg_host, pg_port, pg_user, pg_pwd, kb_name, kb_asset_name, corpus_name):
     pg_url = pg_host+':'+pg_port
@@ -25,6 +26,20 @@ def delete_corpus(pg_host, pg_port, pg_user, pg_pwd, kb_name, kb_asset_name, cor
         vector_itmes_table_name = 'vectorize_items_'+vector_itmes_id
         metadata = MetaData()
         table = Table(vector_itmes_table_name, metadata, autoload_with=engine)
-        delete_stmt = delete(table).where(table.c.source == corpus_name)
-        session.execute(delete_stmt)
-        session.commit()
+        file_name=os.path.splitext(corpus_name)[0]
+        file_type=os.path.splitext(corpus_name)[1]
+        if file_type!='.docx':
+            return
+        query = (
+                select(table.c.source)
+                .where(table.c.source.ilike('%' + file_name+ '%'))
+            )
+        corpus_name_list = session.execute(query).fetchall()
+        for i in range(len(corpus_name_list)):
+            file_name=os.path.splitext(corpus_name_list[i][0])[0]
+            index=file_name[::-1].index('_')
+            file_name=file_name[:len(file_name)-index-1]
+            if file_name+file_type==corpus_name:
+                delete_stmt = delete(table).where(table.c.source == corpus_name_list[i][0])
+                session.execute(delete_stmt)
+                session.commit()

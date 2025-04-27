235235e657b2889973033f5f037f6fae6929a78d
zxstty <zhaojiaqi18@huawei.com>
2025-04-27 07:27:30 +0000

    update run.sh. chat2db服务唤起后，沉睡5s再进行初始化
    Signed-off-by: zxstty <zhaojiaqi18@huawei.com>

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 java -jar tika-server-standard-2.9.2.jar &
 python3 /rag-service/chat2db/app/app.py &
 python3 /rag-service/data_chain/apps/app.py &
+sleep 5
 python3 /rag-service/chat2db/common/init_sql_example.py 
 
 while true

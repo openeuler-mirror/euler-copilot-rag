4f9bac44bb6b659f1039460968b282142c5ed755
zxstty <zhaojiaqi18@huawei.com>
2025-05-15 21:13:18 +0800

    完善service

--- data_chain/apps/service/task_service.py
+++ data_chain/apps/service/task_service.py
@@ -1,107 +1,92 @@
-# Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
-from data_chain.logger.logger import logger as logging
-import traceback
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
 import uuid
-import pytz
-from datetime import datetime
-from data_chain.apps.base.task.document_task_handler import DocumentTaskHandler
-from data_chain.apps.base.task.knowledge_base_task_handler import KnowledgeBaseTaskHandler
-from data_chain.apps.base.task.task_handler import TaskRedisHandler, TaskHandler
+from data_chain.apps.service.task_queue_service import TaskQueueService
+from data_chain.logger.logger import logger as logging
+from data_chain.entities.request_data import (
+    ListTaskRequest
+)
+from data_chain.entities.response_data import (
+    ListTaskMsg)
+from data_chain.entities.enum import TaskType, TaskStatus
+from data_chain.entities.common import default_roles
+from data_chain.stores.database.database import TeamEntity
+from data_chain.apps.base.convertor import Convertor
 from data_chain.manager.task_manager import TaskManager
-from data_chain.models.constant import TaskConstant, TaskActionEnum
-from data_chain.exceptions.exception import TaskException
-from data_chain.config.config import config
+from data_chain.manager.task_report_manager import TaskReportManager
+from data_chain.manager.role_manager import RoleManager
 
 
-async def _validate_task_belong_to_user(user_id: uuid.UUID, task_id: str) -> bool:
-    task_entity = await TaskManager.select_by_id(task_id)
-    if task_entity is None:
-        raise TaskException("Task not exist")
-    if task_entity.user_id != user_id:
-        raise TaskException("Task not belong to user")
-
-
-async def task_queue_handler():
-    # 处理成功的任务
-    success_task_ids = TaskRedisHandler.select_all_task(config['REDIS_SUCCESS_TASK_QUEUE_NAME'])
-    for task_id in success_task_ids:
-        TaskRedisHandler.remove_task_by_task_id(config['REDIS_SUCCESS_TASK_QUEUE_NAME'], task_id)
-        TaskHandler.remove_task(uuid.UUID(task_id))
-        task_entity = await TaskManager.select_by_id(uuid.UUID(task_id))
-        if task_entity is None or task_entity.status != TaskConstant.TASK_STATUS_SUCCESS:
-            TaskRedisHandler.put_task_by_tail(config['REDIS_SILENT_ERROR_TASK_QUEUE_NAME'], task_id)
-    # 处理需要重新开始的任务
-    restart_task_ids = TaskRedisHandler.select_all_task(config['REDIS_RESTART_TASK_QUEUE_NAME'])
-    for task_id in restart_task_ids:
-        TaskRedisHandler.remove_task_by_task_id(config['REDIS_RESTART_TASK_QUEUE_NAME'], task_id)
-        task_entity = await TaskManager.select_by_id(uuid.UUID(task_id))
-        if task_entity is None or task_entity.status != TaskConstant.TASK_STATUS_RUNNING:
-            TaskHandler.remove_task(uuid.UUID(task_id))
-            TaskRedisHandler.put_task_by_tail(config['REDIS_SILENT_ERROR_TASK_QUEUE_NAME'], task_id)
-        else:
-            await TaskHandler.restart_or_clear_task(uuid.UUID(task_id))
-    # 静默处理状态错误的任务
-    silent_error_task_ids = TaskRedisHandler.select_all_task(config['REDIS_SILENT_ERROR_TASK_QUEUE_NAME'])
-    for task_id in silent_error_task_ids:
-        TaskRedisHandler.remove_task_by_task_id(config['REDIS_SILENT_ERROR_TASK_QUEUE_NAME'], task_id)
-        task_entity = await TaskManager.select_by_id(task_id)
-        if task_entity is None:
-            continue
-        elif task_entity.status == TaskConstant.TASK_STATUS_DELETED:
-            await TaskHandler.restart_or_clear_task(task_id, TaskActionEnum.DELETE)
-        elif task_entity.status == TaskConstant.TASK_STATUS_CANCELED:
-            await TaskHandler.restart_or_clear_task(task_id, TaskActionEnum.CANCEL)
-    # 处理等待的任务
-    task_start_cnt = 0
-    while True:
-        task_id = TaskRedisHandler.get_task_by_head(config['REDIS_PENDING_TASK_QUEUE_NAME'])
-        if task_id is None:
-            break
-        task_id = uuid.UUID(task_id)
-        task_entity = await TaskManager.select_by_id(task_id)
-        if task_entity is None or task_entity.status != TaskConstant.TASK_STATUS_PENDING:
-            TaskRedisHandler.put_task_by_tail(config['REDIS_SILENT_ERROR_TASK_QUEUE_NAME'], task_id)
-            continue
+class TaskService:
+    """任务服务"""
+    @staticmethod
+    async def validate_user_action_to_task(
+            user_sub: str, task_id: uuid.UUID, action: str) -> bool:
+        """验证用户在任务中的操作权限"""
         try:
-            func = None
-            # 处理上传资产库任务
-            if task_entity.type == TaskConstant.IMPORT_KNOWLEDGE_BASE:
-                func = KnowledgeBaseTaskHandler.handle_import_knowledge_base_task
-            # 处理打包资产库任务
-            elif task_entity.type == TaskConstant.EXPORT_KNOWLEDGE_BASE:
-                func = KnowledgeBaseTaskHandler.handle_export_knowledge_base_task
-            # 处理临时文件解析任务
-            elif task_entity.type == TaskConstant.PARSE_TEMPORARY_DOCUMENT:
-                func = DocumentTaskHandler.handle_parser_temporary_document_task
-            # 处理文档解析任务
-            elif task_entity.type == TaskConstant.PARSE_DOCUMENT:
-                func = DocumentTaskHandler.handle_parser_document_task
-            if not TaskHandler.add_task(task_id, target=func, t_id=task_entity.id):
-                TaskRedisHandler.put_task_by_tail(config['REDIS_PENDING_TASK_QUEUE_NAME'], str(task_id))
-                break
-            await TaskManager.update(task_id, {'status': TaskConstant.TASK_STATUS_RUNNING})
+            task_entity = await TaskManager.get_task_by_task_id(task_id)
+            if task_entity is None:
+                logging.exception("[TaskService] 任务不存在")
+                raise Exception("Task not exist")
+            action_entity = await RoleManager.get_action_by_team_id_user_sub_and_action(
+                user_sub, task_entity.team_id, action)
+            if action_entity is None:
+                return False
+            return True
         except Exception as e:
-            logging.error(f"Handle task {task_id} error: {e}")
-            await TaskHandler.restart_or_clear_task(task_id)
-            break
-        task_start_cnt += 1
-        if task_start_cnt == 8:
-            break
+            err = "验证用户在任务中的操作权限失败"
+            logging.exception("[TaskService] %s", err)
+            raise e
 
+    async def list_task(user_sub: str, req: ListTaskRequest) -> ListTaskMsg:
+        """列出任务"""
+        try:
+            total, task_entities = await TaskManager.list_task(user_sub, req)
+            tasks = []
+            task_ids = [task_entity.id for task_entity in task_entities]
+            task_report_entities = await TaskReportManager.list_current_task_report_by_task_ids(task_ids)
+            task_report_dict = {task_report_entity.task_id: task_report_entity
+                                for task_report_entity in task_report_entities}
+            for task_entity in task_entities:
+                task_report = task_report_dict.get(task_entity.id, None)
+                task = await Convertor.convert_task_entity_to_task(task_entity, task_report)
+                tasks.append(task)
+            return ListTaskMsg(total=total, tasks=tasks)
+        except Exception as e:
+            err = "列出任务失败"
+            logging.exception("[TaskService] %s", err)
+            raise e
 
-async def monitor_tasks():
-    task_id_list = TaskHandler.list_tasks()
-    for task_id in task_id_list:
-        task_entity = await TaskManager.select_by_id(task_id)
-        if task_entity is None:
-            continue
-        current_time_utc = datetime.now(pytz.utc)
-        time_difference = current_time_utc - task_entity.created_time
-        seconds = time_difference.total_seconds()
-        if seconds > 12*3600:
-            if task_entity.status==TaskConstant.TASK_STATUS_DELETED:
-                await TaskHandler.restart_or_clear_task(task_id,TaskActionEnum.DELETE)
-            elif task_entity.status==TaskConstant.TASK_STATUS_CANCELED:
-                await TaskHandler.restart_or_clear_task(task_id,TaskActionEnum.CANCEL)
-            else:
-                await TaskHandler.restart_or_clear_task(task_id)
+    @staticmethod
+    async def delete_task_by_task_id(
+            task_id: uuid.UUID) -> uuid.UUID:
+        """根据任务ID删除任务"""
+        try:
+            task_entity = await TaskManager.get_task_by_task_id(task_id)
+            if task_entity is None:
+                err = "任务不存在"
+                logging.exception("[TaskService] %s", err)
+                raise Exception(err)
+            task_id = await TaskQueueService.delete_task(task_id)
+            return task_id
+        except Exception as e:
+            err = "删除任务失败"
+            logging.exception("[TaskService] %s", err)
+            raise e
+
+    @staticmethod
+    async def delete_task_by_type(
+            user_sub: str, team_id: uuid.UUID, task_type: TaskType) -> list[uuid.UUID]:
+        """根据任务类型删除任务"""
+        try:
+            task_entities = await TaskManager.list_task_by_user_sub_and_team_id_and_type(
+                user_sub, team_id, task_type)
+            task_ids = []
+            for task_entity in task_entities:
+                task_id = await TaskQueueService.delete_task(task_entity.id)
+                if task_id is not None:
+                    task_ids.append(task_id)
+            return task_ids
+        except Exception as e:
+            err = "删除任务失败"
+            logging.exception("[TaskService] %s", err)
+            raise e
---
+++ data_chain/apps/service/team_service.py
@@ -0,0 +1,102 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+import uuid
+from data_chain.logger.logger import logger as logging
+from data_chain.entities.request_data import ListTeamRequest, CreateTeamRequest
+from data_chain.entities.response_data import ListTeamMsg
+from data_chain.entities.enum import TeamType, TeamStatus
+from data_chain.entities.common import default_roles
+from data_chain.stores.database.database import TeamEntity
+from data_chain.apps.base.convertor import Convertor
+from data_chain.manager.team_manager import TeamManager
+from data_chain.manager.role_manager import RoleManager
+
+
+class TeamService:
+    """团队服务"""
+    @staticmethod
+    async def validate_user_action_in_team(
+            user_sub: str, team_id: uuid.UUID, action: str) -> bool:
+        """验证用户在团队中的操作权限"""
+        try:
+            action_entity = await RoleManager.get_action_by_team_id_user_sub_and_action(user_sub, team_id, action)
+            if action_entity is None:
+                return False
+            return True
+        except Exception as e:
+            err = "验证用户在团队中的操作权限失败"
+            logging.exception("[TeamService] %s", err)
+            raise e
+
+    @staticmethod
+    async def list_teams(user_sub: str, req: ListTeamRequest) -> ListTeamMsg:
+        """列出团队"""
+        if req.team_type == TeamType.MYCREATED:
+            total, team_entities = await TeamManager.list_team_mycreated_user_sub(user_sub, req)
+        elif req.team_type == TeamType.MYJOINED:
+            total, team_entities = await TeamManager.list_team_myjoined_by_user_sub(user_sub, req)
+        else:
+            total, team_entities = await TeamManager.list_pulic_team(user_sub, req)
+        teams = []
+        for team_entity in team_entities:
+            team = await Convertor.convert_team_entity_to_team(team_entity)
+            teams.append(team)
+        return ListTeamMsg(total=total, teams=teams)
+
+    @staticmethod
+    async def create_team(user_sub: str, req: CreateTeamRequest) -> uuid.UUID:
+        """创建团队"""
+        try:
+            team_entity = await Convertor.convert_create_team_request_to_team_entity(user_sub, req)
+            team_entity = await TeamManager.add_team(team_entity)
+            team_user_entity = await Convertor.convert_user_sub_and_team_id_to_team_user_entity(user_sub, team_entity.id)
+            await TeamManager.add_team_user(team_user_entity)
+            become_creator_flag = False
+            for role_dict in default_roles:
+                role_entity = await Convertor.convert_default_role_dict_to_role_entity(team_entity.id, role_dict)
+                role_entity = await RoleManager.add_role(role_entity)
+                if not become_creator_flag:
+                    user_role_entity = await Convertor.convert_user_sub_role_id_and_team_id_to_user_role_entity(
+                        user_sub, role_entity.id, team_entity.id)
+                    await RoleManager.add_user_role(user_role_entity)
+                    become_creator_flag = True
+                role_action_entities = await Convertor.convert_default_role_action_dicts_to_role_action_entities(role_entity.id, role_dict['actions'])
+                await RoleManager.add_role_actions(role_action_entities)
+            return team_entity.id
+        except Exception as e:
+            err = "创建团队失败"
+            logging.exception("[TeamService] %s", err)
+            raise e
+
+    @staticmethod
+    async def update_team_by_team_id(
+            user_sub: str, team_id: uuid.UUID, req: CreateTeamRequest) -> bool:
+        """更新团队"""
+        try:
+            team_dict = await Convertor.convert_update_team_request_to_dict(req)
+            team_entity = await TeamManager.update_team_by_id(team_id, team_dict)
+            if team_entity is None:
+                err = "更新团队失败"
+                logging.exception("[TeamService] %s", err)
+                raise "更新团队失败, 团队不存在"
+            return team_entity.id
+        except Exception as e:
+            err = "更新团队失败"
+            logging.exception("[TeamService] %s", err)
+            raise e
+
+    @staticmethod
+    async def soft_delete_team_by_team_id(
+            team_id: uuid.UUID) -> bool:
+        """软删除团队"""
+        try:
+            team_entity = await TeamManager.update_team_by_id(
+                team_id, {"status": TeamStatus.DELETED.value})
+            if team_entity is None:
+                err = "软删除团队失败"
+                logging.exception("[TeamService] %s", err)
+                raise "软删除团队失败, 团队不存在"
+            return team_entity.id
+        except Exception as e:
+            err = "软删除团队失败"
+            logging.exception("[TeamService] %s", err)
+            raise e

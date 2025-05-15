8e6b565cd0bf6bd48110206297660bb197e1d7ac
zxstty <zhaojiaqi18@huawei.com>
2025-05-15 20:50:27 +0800

    增加base worker

--- data_chain/apps/base/task/worker/base_worker.py
+++ data_chain/apps/base/task/worker/base_worker.py
@@ -1,6 +1,10 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
 import uuid
-
+import importlib
+import os
+import sys
+import inspect
+from pathlib import Path
 from data_chain.apps.base.task.process_handler import ProcessHandler
 from data_chain.config.config import config
 from data_chain.entities.enum import TaskStatus
@@ -29,27 +33,29 @@ class BaseWorker:
     @staticmethod
     async def get_worker_name(task_id: uuid.UUID) -> str:
         '''获取worker_name'''
-        task = TaskManager.get_task_by_id(task_id)
-        if task is None:
-            err = f"[BaseWorker] 任务不存在，task_id: {task_id}"
-            logging.exception(err)
-            raise err
-        return task.type
+        task_entity = await TaskManager.get_task_by_task_id(task_id)
+        if task_entity is None:
+            err = f"获取任务失败, 任务ID: {task_id}"
+            logging.error("[BaseWorker] %s", err)
+            raise ValueError(err)
+        return task_entity.type
 
     @staticmethod
     async def init(worker_name: str, op_id: uuid.UUID) -> uuid.UUID:
         '''初始化任务'''
         task_id = await (BaseWorker.find_worker_class(worker_name).init(op_id))
         await TaskManager.update_task_by_id(task_id, {"status": TaskStatus.PENDING.value})
+        return task_id
 
     @staticmethod
     async def reinit(task_id: uuid.UUID) -> bool:
         '''重新初始化任务'''
         worker_name = await BaseWorker.get_worker_name(task_id)
         flag = await (BaseWorker.find_worker_class(worker_name).reinit(task_id))
-        task_entity = await TaskManager.get_task_by_id(task_id)
+        task_entity = await TaskManager.get_task_by_task_id(task_id)
+        ProcessHandler.remove_task(task_id)
         if flag:
-            TaskManager.update_task_by_id(task_id, {"status": TaskStatus.PENDING.value, "retry": task_entity.retry + 1})
+            await TaskManager.update_task_by_id(task_id, {"status": TaskStatus.PENDING.value, "retry": task_entity.retry + 1})
             return True
         else:
             await TaskManager.update_task_by_id(task_id, {"status": TaskStatus.FAILED.value})
@@ -59,6 +65,7 @@ class BaseWorker:
     async def deinit(task_id: uuid.UUID) -> uuid.UUID:
         '''析构任务'''
         worker_name = await BaseWorker.get_worker_name(task_id)
+        ProcessHandler.remove_task(task_id)
         await (BaseWorker.find_worker_class(worker_name).deinit(task_id))
         await TaskManager.update_task_by_id(task_id, {"status": TaskStatus.SUCCESS.value})
 
@@ -66,7 +73,7 @@ class BaseWorker:
     async def run(task_id: uuid.UUID) -> bool:
         '''运行任务'''
         worker_name = await BaseWorker.get_worker_name(task_id)
-        flag = ProcessHandler.add_task(BaseWorker.find_worker_class(worker_name).run, task_id)
+        flag = ProcessHandler.add_task(task_id, BaseWorker.find_worker_class(worker_name).run, task_id)
         await TaskManager.update_task_by_id(task_id, {"status": TaskStatus.RUNNING.value})
         return flag
 
@@ -74,9 +81,9 @@ class BaseWorker:
     async def stop(task_id: uuid.UUID) -> bool:
         '''停止任务'''
         worker_name = await BaseWorker.get_worker_name(task_id)
-        task_entity = await TaskManager.get_task_by_id(task_id)
+        task_entity = await TaskManager.get_task_by_task_id(task_id)
         if task_entity.status == TaskStatus.RUNNING.value:
-            await ProcessHandler.remove_task(task_id)
+            ProcessHandler.remove_task(task_id)
         elif task_entity.status == TaskStatus.PENDING.value:
             await TaskQueueManager.delete_task_by_id(task_id)
         else:
